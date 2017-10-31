491c8e25c86908d5a667bb4d2671e4ec4ed2348a
Jevgenij Solovjov <jsolov@gmail.com>
2017-10-31 19:03:16 +0800

    More types explicitly counted, and all 'weird' types also counted.

--- lib/marshal/structure/explorer.rb
+++ lib/marshal/structure/explorer.rb
@@ -78,14 +78,20 @@ class Marshal::Structure   #::Explorer
     end
 
     def get_nonprimitive_object_counts(struc_arr)
-      sym_table = {} #get_symbols_hash_by_id(struc_arr)
+      sym_table = get_symbols_hash_by_id(struc_arr)
       result = {}
-      
+
       yield_and_recursively_traverse_structure_array(struc_arr) do |depth, el_type, el_id, curr_ary|
-        # in properly formed marshalled data symbol definition clearly must precede its linked use
+        # # in properly formed marshalled data symbol definition clearly must precede its linked use
         if el_type == :symbol
-          sym_name = curr_ary[2]
-          sym_table[el_id] = sym_name.to_sym
+        #   sym_name = curr_ary[2]
+        #   sym_table[el_id] = sym_name.to_sym
+        # els
+          result[:symbol] = 0 if !result[:symbol]
+          result[:symbol] += 1
+        elsif el_type == :symbol_link
+          result[:symbol_link] = 0 if !result[:symbol_link]
+          result[:symbol_link] += 1
         elsif el_type == :object
           class_info = curr_ary[2]
           name_typ, name_symbol_id = class_info[0..1]
@@ -94,6 +100,39 @@ class Marshal::Structure   #::Explorer
 
           result[class_name] = 0 if !result[class_name]
           result[class_name] += 1
+        elsif el_type == :user_class
+          class_info = curr_ary[1]
+          name_typ, name_symbol_id = class_info[0..1]
+          raise "Unexpected Object name type ':#{name_typ}', only know how to handle :symbol or :symbol_link" unless name_typ == :symbol || name_typ == :symbol_link
+          class_name = sym_table[name_symbol_id]
+
+          result[class_name] = 0 if !result[class_name]
+          result[class_name] += 1
+        elsif el_type == :array
+          result[:Array] = 0 if !result[:Array]
+          result[:Array] += 1
+        elsif el_type == :hash
+          result[:Hash] = 0 if !result[:Hash]
+          result[:Hash] += 1
+        elsif el_type == :hash_default
+          # combine with ordinary hashes
+          result[:Hash] = 0 if !result[:Hash]
+          result[:Hash] += 1
+        elsif el_type == :float
+          result[:float] = 0 if !result[:float]
+          result[:float] += 1
+        elsif el_type == :fixnum
+          result[:fixnum] = 0 if !result[:fixnum]
+          result[:fixnum] += 1
+        elsif el_type == :string
+          result[:string] = 0 if !result[:string]
+          result[:string] += 1
+        #elsif el_type == :instance_variables
+          # ignore - this one normally references other objects
+        else
+          result[:encountered_types_unsure_how_to_count] = {} unless result[:encountered_types_unsure_how_to_count]
+          result[:encountered_types_unsure_how_to_count][el_type] = 0 unless result[:encountered_types_unsure_how_to_count][el_type]
+          result[:encountered_types_unsure_how_to_count][el_type] += 1
         end
       end
       result
